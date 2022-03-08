d01f4b58c273f3469ec2a2ff251731eb69321527
Lucie Galland <lucie.galland@ens.fr>
2022-03-08 14:17:07 +0100

    f

--- main.py
+++ main.py
@@ -16,24 +16,29 @@ if __name__ == '__main__':
     entropies = []
     test = []
     cumul_rewards =[]
-    for i in range(100):
+    for i in range(2):
         emotion_error, emotion_acc, belief_error,action, cumul_reward = env.interaction(verbose = True)
         #mean_emotion_error += [np.mean(emotion_error)]
         #actions += action
         #belief_errors += [np.mean(belief_error)]
         #emotion_errors += [np.mean(emotion_error)]
         #emotion_accs += [np.mean(emotion_acc)]
-        #entropies.append(-np.sum(entropy(env.agent.mean_transitions)))
+        entropies.append(-np.sum(entropy(env.agent.mean_transitions)))
         #test.append(np.mean(np.array(env.agent.emotion_transitions)[:,ActionType.SELF_DISCLOSE,EmotionType.LOVE]))
         cumul_rewards.append(cumul_reward)
 
 
-    plt.plot(cumul_rewards, label="reward cumul")
+        plt.plot(cumul_rewards, label="reward cumul")
+        plt.legend()
+        plt.savefig("reward.png")
+        plt.show()
+
     #plt.plot(actions,label = "action")
     #plt.plot(emotion_errors,label = "emotion error")
     #plt.plot(belief_errors, label ="belief error")
-    #plt.plot(entropies/np.mean(entropies), label="entropy")
+        plt.plot(entropies/np.mean(entropies), label="entropy")
     #plt.plot(test, label ="test")
     #plt.plot(emotion_accs, label = "emotion accuracy")
-    plt.legend()
-    plt.show()
+        plt.legend()
+        plt.savefig("entropy.png")
+        plt.show()
